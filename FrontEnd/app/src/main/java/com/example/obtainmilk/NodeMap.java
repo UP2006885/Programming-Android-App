b6e7a0c8cef0ab3e12e10d909c6f79a8509c6a9f
UP2006885 <91878094+UP2006885@users.noreply.github.com>
2022-01-26 17:17:17 +0000

    Added backend code

---
+++ FrontEnd/app/src/main/java/com/example/obtainmilk/NodeMap.java
@@ -0,0 +1,51 @@
+package com.example.obtainmilk;
+import java.io.FileNotFoundException;
+
+public class NodeMap {
+    // Variables
+    private Node head;
+    private Node currentNode;
+
+    // Getters/Setters
+    public Node currentNode() { return currentNode;}
+    public void noDecision(){
+        currentNode = currentNode.getYesNode();
+    }
+
+    // Switch statement for user input
+    public void decision(int inputID) {
+        switch (inputID) {
+            case 1 -> currentNode = currentNode.getYesNode();
+            case 2 -> currentNode = currentNode.getNoNode();
+            case 3 -> currentNode = currentNode.getOptionalNode();
+        }
+    }
+
+    // Building Map
+    public NodeMap()  {
+        NodeCollection nodeCollection;
+        try {
+            nodeCollection = new NodeCollection();
+            head = nodeCollection.get(0);
+        } catch (FileNotFoundException e) {
+            //message
+            return;
+        }
+        buildMap(nodeCollection);
+        currentNode = head;
+    }
+    private void buildMap(NodeCollection NodeCollection)   {
+        if (NodeCollection == null) {return;}
+        for(Node source : NodeCollection.arrayList()){
+            int yesID = source.getYesID();
+            int noID = source.getNoID();
+            int optionalID = source.getOptionalID();
+            Node yesNode = NodeCollection.locateNodeBy(yesID);
+            Node noNode = NodeCollection.locateNodeBy(noID);
+            Node optionalNode = NodeCollection.locateNodeBy(optionalID);
+            source.setYesNode(yesNode);
+            source.setNoNode(noNode);
+            source.setOptionalNode(optionalNode);
+        }
+    }
+}
